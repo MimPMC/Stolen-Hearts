1294ab9721fbd320508c534200cb2833472ef656
mims <113519976+MimPMC@users.noreply.github.com>
2023-03-08 00:27:15 +0100

    create new game

--- src/main.ts
+++ src/main.ts
@@ -1,154 +1,38 @@
 export { };
   import Character from "./Character";
-  import GameButton from "./GameButton";
-  import Item from "./Item";
-  import TextBubble from "./TextBubble";
 
-window.addEventListener("DOMContentLoaded", startTest);
+window.addEventListener("DOMContentLoaded", startGame);
 
-function startTest() {
-  const app: HTMLBodyElement | null = document.querySelector(
-    "#app"
-  ) as HTMLBodyElement;
+type ImageSources = {
+  city: string;
+  hall: string;
+  pinkRoom: string;
+  alice1: string;
+  jankos:string;
+  elias1:string;
+  elias2:string;
+};
 
-  type ImageSources = {
-    city: string;
-    fisk: string;
-    home: string;
-  };
+const imageSources: ImageSources = {
+  city: "/assets/centrum_bg.png",
+  hall: "/assets/hall.png",
+  pinkRoom: "/assets/bedroom_pink.png",
+  alice1: "/assets/Alice/alice-1.png",
+  jankos: "/assets/jankos/jankos_talking.png",
+  elias1: "/assets/Elias/elias-sprite-1.png",
+  elias2: "/assets/Elias/elias-sprite-1.png"
+};
 
-  const imageSources: ImageSources = {
-    city: "/assets/centrum_bg.png",
-    fisk: "/assets/fisk.png",
-    home: "/assets/hall.png",
-  };
+const aliceImages = [ imageSources.alice1,imageSources.alice1]
+const jankosImages = [ imageSources.jankos,imageSources.jankos]
+const eliasImages = [ imageSources.elias1,imageSources.elias2]
 
-  const background: HTMLDivElement = document.createElement("div");
-  if (imageSources.city) {
-    background.style.backgroundImage = `url(${imageSources.city})`;
-    app?.appendChild(background);
-    background.className = "background";
-  }
-
-  const startButton = new GameButton(
-    { x: 20, y: 20 },
-    { x: 20, y: 50 },
-    "Delete Martin",
-    "standard-button"
-  );
-  startButton.draw(background, function () {
-    jankos.erase();
-    jankosTextBubble.erase();
-  });
-
-  const choice = new GameButton(
-    { x: 20, y: 30 },
-    { x: 20, y: 50 },
-    "Make Martin sad",
-    "choice-button"
-  );
-  const blushButton = new GameButton(
-    { x: 20, y: 40 },
-    { x: 20, y: 50 },
-    "Make Martin surprised",
-    "choice-button"
-  );
-  blushButton.draw(background, function () {
-    jankos.update(5);
-    jankosTextBubble.updateText("What????")
-
-  });
-
-  choice.draw(background, handleChoiceButtonClick);
-
-  const character1Images = [
-    "/assets/jankos_talking.png",
-    "/assets/jankos_worry.png",
-    "/assets/jankos_blush1.png",
-    "/assets/jankos_cry.png",
-    "/assets/jankos_smirk.png",
-    "/assets/jankos_surprised.png",
-  ];
-  const jankos = new Character(60, 90, character1Images);
-  jankos.draw(background);
-
- 
-
-  function handleChoiceButtonClick() {
-    jankos.update(1);
-    const cryButton = new GameButton(
-      { x: 20, y: 60 },
-      { x: 20, y: 50 },
-      "make Martin cry",
-      "standard-button"
-    );
-    jankosTextBubble.updateText("That was very mean :(")
-    cryButton.draw(background, function () {
-      jankos.update(3);
-      jankosTextBubble.updateText("WUUÄÄH")
-
-    });
-  }
-  const resetButton = new GameButton(
-    { x: 20, y: 50 },
-    { x: 20, y: 50 },
-    "reset",
-    "standard-button"
-  );
-  resetButton.draw(background, function () {
-    app.innerHTML = "";
-    startGame();
-  });
-
-  const homeButton = new GameButton(
-    { x: 20, y: 70 },
-    { x: 20, y: 50 },
-    "Go home",
-    "standard-button"
-  );
-  homeButton.draw(background, function () {
-    background.style.backgroundImage = `url(${imageSources.home})`;
-    jankosTextBubble.updateText("We are home")
-    fish.useItem(useMyItem);
-  });
-
-  const jankosTextBubble = new TextBubble( { x: 5, y: 83},
-    { x: 90, y: 15 },
-    "text-bubble",
-    "Hey, what's up?",
-    "Martin")
-  jankosTextBubble.draw(background)
-
-
-  const fish = new Item({ x: 26, y: 43},
-    { x: 3, y: 15 },
-    "A fish",
-    imageSources.fisk,
-    false)
-
-  fish.draw(background)
-  
-
-}
 
 function startGame() {
   const app: HTMLBodyElement | null = document.querySelector(
     "#app"
   ) as HTMLBodyElement;
 
-  type ImageSources = {
-    city: string;
-    fisk: string;
-    home: string;
-    pinkRoom: string;
-  };
-
-  const imageSources: ImageSources = {
-    city: "/assets/centrum_bg.png",
-    fisk: "/assets/fisk.png",
-    home: "/assets/hall.png",
-    pinkRoom: "/assets/bedroom_pink.png"
-  };
   const background: HTMLDivElement = document.createElement("div");
   if (imageSources.city) {
     background.style.backgroundImage = `url(${imageSources.pinkRoom})`;
@@ -156,10 +40,14 @@ function startGame() {
     background.className = "background";
   }
 
+  const alice = new Character(50,85, aliceImages)
+  alice.draw(background)
+
+  const jankos = new Character(30,90, jankosImages)
+  jankos.draw(background)
+
+  const elias = new Character(70,95, eliasImages)
+  elias.draw(background)
+
 }
 
-
-
-function useMyItem(item: Item) {
-  console.log(`Using ${item.description}!`);
-}
